54c7dfb6b91c78243257b9978aa98bb5cb25a11c
Minterr02 <ananyaporn.minterr@gmail.com>
2023-10-26 13:02:13 +0700

    edit from B

--- index.js
+++ index.js
@@ -5,12 +5,9 @@ function feature1() {
 function feature2() {
     //comment from B
     alert('from feature2');
-<<<<<<< HEAD
-    alert('from B')
-=======
+    alert('from B');
 }
 
 function feature3() {
     alert('from feature3');
->>>>>>> refs/remotes/origin/main
 }
